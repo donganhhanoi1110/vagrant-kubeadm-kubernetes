685dfe848d35bd5f43b9c9965cd61844fd94f830
Maxx <donganhhanoi1110@gmail.com>
2023-08-10 13:11:26 +0700

    Update node longhorn install iscsi

--- scripts/node.sh
+++ scripts/node.sh
@@ -16,3 +16,7 @@ sudo chown 1000:1000 /home/vagrant/.kube/config
 NODENAME=$(hostname -s)
 kubectl label node $(hostname -s) node-role.kubernetes.io/worker=worker
 EOF
+
+#install longhorn StorageClass
+sudo mkdir -p /data/longhorn-storage
+sudo apt install open-iscsi
